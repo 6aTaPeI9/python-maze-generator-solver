addbcaf8a2a00cfe6106b6d6cc0361997b49ffc1
ad.radchenko <ad.radchenko@tensor.ru>
2020-05-12 23:26:55 +0300

    Доработан виджет, авто скейлинг по ширине и высоте.

--- QMazeWidget.py
+++ QMazeWidget.py
@@ -12,7 +12,7 @@ class QMazeVisualizer(QWidget):
 
     def paintEvent(self, event):
         """
-            Метод перерисовывющий виджет
+            Событие перерисовки окна
         """
         qp = QPainter()
         qp.begin(self)
@@ -21,4 +21,29 @@ class QMazeVisualizer(QWidget):
 
 
     def drawWidget(self, qp):
-        pass
+        """
+            Метод перерисовывющий виджет
+        """
+        qp.setRenderHint(QPainter.Antialiasing)
+        qp.setPen(Qt.gray)
+        qp.setBrush(Qt.white)
+
+        parent_size = self.size()
+        height_padding = (self.maze.height * 1) # оступ между клетками по ширине
+        width_padding = (self.maze.width * 1) # отступ между клетками по высотке
+        bar_width = (parent_size.width() - width_padding) // self.maze.width # масштабированная ширина клетки
+        bar_height = (parent_size.height() - height_padding) // self.maze.height # масштабированная высота клетки
+
+        for col in range(self.maze.height):
+            for row in range(self.maze.width):
+                ceil = self.maze.matrix[col][row]
+                qp.setBrush(ceil.color)
+                qp.drawRect(
+                    row * bar_width,
+                    col * bar_height,
+                    bar_width,
+                    bar_height
+                )
+
+        return
+
--- maze_generators/recursive_backtracker.py
+++ maze_generators/recursive_backtracker.py
@@ -2,11 +2,11 @@ from maze_generators import maze_matrix
 
 
 class RecursiveBacktracker(maze_matrix.Maze):
-    def __init__(self, width, heigth):
+    def __init__(self, width, height):
         self.title = 'Recursive Backtracker'
         self.width = width
-        self.heigth = heigth
-        self.matrix = maze_matrix.gen_maze_matrix(self.width, self.heigth)
+        self.height = height
+        self.matrix = maze_matrix.gen_maze_matrix(self.width, self.height)
 
     def generate_maze(self):
         pass
